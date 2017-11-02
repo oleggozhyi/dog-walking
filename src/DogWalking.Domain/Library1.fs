291abd9f5f5788597bed57558e2e6b3dcfbd4914
Oleg Gozhyi <oleg.gozhyi@outlook.com>
2017-11-02 23:01:43 +0200

    started modeling domain

---
+++ src/DogWalking.Domain/Library1.fs
@@ -0,0 +1,23 @@
+﻿namespace DogWalking.Domain
+open System
+
+type Email = Email of string
+type Name = Name of string
+
+
+type Dog = {
+    Id: Guid;
+    Name: string;
+    Small: bool;
+    Aggressive: bool
+}
+
+type Customer = {
+    Id: Guid;
+    Name: string;
+    Phone: string;
+    Email: string;
+    Dogs: Dog list
+}
+type Pack =  Pack of Dog list
+
